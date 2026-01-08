ddb3ec1208fceb26b7536777fdab01b3ee75c956
shibanand01 <shibanand3787@gmail.com>
2026-01-09 03:02:58 +0530

    project setup

---
+++ Cinephile/src/main/java/com/shiv/Cinephile/CinephileApplication.java
@@ -0,0 +1,13 @@
+package com.shiv.Cinephile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinephileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinephileApplication.class, args);
+	}
+
+}
